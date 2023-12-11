136b10b13e12e0e13d223ecb658ec6aaf319bafb
Stephanos <bjoekeldude@users.noreply.github.com>
2023-12-11 19:36:56 +0100

    Update write-to-json2.cpp

--- write-to-json2.cpp
+++ write-to-json2.cpp
@@ -19,18 +19,18 @@ struct myType_t{
 
 
 int main(int argc, char** argv){
-
+    std::ofstream outputfile{argv[1]};
+    if (!outputfile) std::abort;
+    
     myType_t beta{
         .a{10},
         .b{30}
     };
 
-    nlohmann::json output = beta.serialize();
+    nlohmann::json output{beta.serialize()};
 
-    std::ofstream out(argv[1]);
-
-    out << output;
-    out.flush();
-    out.close();
+    outputfile << output;
+    outputfile.flush();
+    outputfile.close();
 }
 
